2751b1b597c731def904b743179a787333428fa8
Paulie <64064864+PaulieNor@users.noreply.github.com>
2024-09-05 18:49:57 +0100

    Delete k8scaledownk8s.sh

--- k8scaledownk8s.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -e
-aws eks update-kubeconfig --name 'dev-eks-cluster'
-helm uninstall jom 
-terraform -chdir='jom_tfm/tfm/dev' destroy -target='module.eks' -var='account_id=798011338410' -auto-approve
-echo 
-echo Resources decommissioned.
-echo
